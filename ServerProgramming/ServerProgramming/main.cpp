c13a7b39275318e6758f76c1fd0be499a1ee4098
ohsd <erfrr@naver.com>
2022-09-13 22:00:46 +0900

    [게임서버 프로그래밍] ** 체스판 만들기 **
    -기본 윈도우창을 생성해서 출력한 후 CChessGame 클래스 변수 maingame을 선언.
    - 윈도우 프로시저에서 메세지에 맞게 maingame에서 수행해야 할 적절한 멤버 함수들을 작성

---
+++ ServerProgramming/ServerProgramming/main.cpp
@@ -0,0 +1,78 @@
+#include "ChessGame.h"
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("HomeWork1");
+CChessGame maingame;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;   // 윈도우 핸들
+	MSG Message; // 메세지
+	g_hInst = hInstance; // 인스턴스 핸들을 다른 프로세스에서도 사용할 수 있도록 전역변수에 대입
+
+	// 윈도우 클래스(윈도우의 속성을 정의하는 구조체)를 초기화
+	WNDCLASS wc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)(COLOR_BTNFACE + 1);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpszClassName = lpszClass;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_VREDRAW | CS_HREDRAW;
+	RegisterClass(&wc); // 초기화 시킨 윈도우 클래스를 운영체제에 등록
+
+	// 윈도우 클래스 바탕으로 윈도우창을 생성한다
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 610, 630
+		, NULL, NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow); // 윈도우창을 출력
+	if (hWnd == NULL) return -1;
+
+	// 메세지 루프
+	while (GetMessage(&Message, NULL, 0, 0) > 0) { // 메세지 큐에 있는 메세지들을 확인
+		TranslateMessage(&Message); // 키보드에 입력된 메세지를 인스턴스가 이해하기 쉬운 형태로 해석
+		DispatchMessage(&Message); // WndProc으로 해석시킨 메세지를 전달한다.
+	}
+
+	return (int)Message.wParam;
+}
+
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+	PAINTSTRUCT ps;
+	switch (iMessage) {
+	case WM_CREATE:
+		maingame.InitStage(g_hInst);
+		break;
+	case WM_KEYDOWN:
+		switch (wParam) {
+		case VK_SPACE:
+			maingame.SaveGame();
+			break;
+		case VK_DOWN:
+		case VK_UP:
+		case VK_RIGHT:
+		case VK_LEFT:
+			maingame.Action(wParam);
+			break;
+		}
+		InvalidateRect(hWnd, NULL, FALSE);
+		break;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		maingame.DrawScreen(hdc);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
