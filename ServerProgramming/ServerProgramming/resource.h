cf52cbf4186a0e15b9e71772cf098f636a9f32f5
ohsd <erfrr@naver.com>
2022-09-13 23:25:39 +0900

    [게임서버 프로그래밍] ** 체스판 서버와 연결하기 **
    - 응용 프로그램 실행 시 IP 주소를 입력 받는 다이얼로그 리소스 생성

--- ServerProgramming/ServerProgramming/resource.h
+++ ServerProgramming/ServerProgramming/resource.h
@@ -4,14 +4,17 @@
 //
 #define IDB_BITMAP1                     103
 #define IDB_BITMAP2                     104
+#define IDD_DIALOG1                     105
+#define IDC_EDIT1                       1001
+#define IDC_EDIT_IPADDR                 1001
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
